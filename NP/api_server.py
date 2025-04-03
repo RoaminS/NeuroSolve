fee529925d97059d95e9f0a0fc403bdc019551bb
Romain <consulting@web3-crypto.xyz>
2025-04-03 10:47:50 +0200

    api_server.py

---
+++ NP/api_server.py
@@ -0,0 +1,27 @@
+'''
+➡️ Lance le serveur avec :
+python api_server.py
+
+➡️ Puis dans un autre terminal :
+python ns018_api_push.py
+'''
+
+# api_server.py
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/receive_prediction", methods=["POST"])
+def receive_prediction():
+    data = request.get_json()
+    print("📥 Prédictions reçues :", len(data["data"]))
+    return "Prédictions reçues"
+
+@app.route("/receive_alert", methods=["POST"])
+def receive_alert():
+    data = request.get_json()
+    print("📥 Alertes reçues :", len(data["data"]))
+    return "Alertes reçues"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=6000)
